338f026509deef9bc8248ebf35d739e8d482c763
Paul Jablonski <pauljablonski@me.com>
2017-08-18 21:04:02 +0100

    Add debounce to search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
+import _ from 'lodash'
 
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
@@ -17,18 +18,22 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     }
+    this.videoSearch("Surfboards")
   }
 
-  videoSearch(term){    
-    YTSearch({key: API_KEY, term:"Surfboards"}, (videos) => {
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({ videos, selectedVideo: videos[0] })
     })
   }
 
   render(){
+
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+
     return(
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={ videoSearch }/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           videos={this.state.videos}
